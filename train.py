81d262ed8c5b194645b82987ef1baec7322b4164
skywalkerylh <r11528050@g.ntu.edu.tw>
2024-03-21 22:53:42 +0800

    add one-hot, only_prediction

--- train.py
+++ train.py
@@ -1,6 +1,7 @@
 import torch
 import numpy as np
 from utils import metrics, metrics_individual, plot
+import torch.nn.functional as F
 
 class DomainAdaptationTrainer:
     def __init__(self, feature_extractor, label_predictor, domain_classifier, \
@@ -43,39 +44,47 @@ class DomainAdaptationTrainer:
             for i, ((source_data, source_label), (target_data, _)) in enumerate(zip(self.dataloader_source, self.dataloader_target)):
 
                 source_data = source_data.cuda()
-                source_label = source_label.cuda()
+                source_label=F.one_hot(source_label, num_classes=2)
+                source_label= source_label.type(torch.FloatTensor).cuda()
+                #source_label = source_label.cuda()
                 target_data = target_data.cuda()
                 
                 # 我們把source data和target data混在一起，否則batch_norm可能會算錯 (兩邊的data的mean/var不太一樣)
                 mixed_data = torch.cat([source_data, target_data], dim=0)
-                domain_label = torch.zeros([source_data.shape[0] + target_data.shape[0], 1]).cuda()
+                #domain_label = torch.zeros([source_data.shape[0] + target_data.shape[0], 1], dtype=torch.int64).cuda()
+                domain_label = torch.zeros([source_data.shape[0] + target_data.shape[0]], dtype=torch.long)
                 # 設定source data的label為1
                 domain_label[:source_data.shape[0]] = 1
-
+                domain_label=F.one_hot(domain_label, num_classes=2)
+                domain_label= domain_label.type(torch.FloatTensor).cuda()
+               
                 # Step 1 : 訓練Domain Classifier
                 feature_extractor = self.feature_extractor(mixed_data)
                 # 因為我們在Step 1不需要訓練Feature Extractor，所以把feature detach避免loss backprop上去。
                 domain_logits = self.domain_classifier(feature_extractor.detach())
-                loss = self.domain_criterion(domain_logits, domain_label)
-                running_D_loss+= loss.item()
-                loss.backward()
+                
+                d_loss = self.domain_criterion(domain_logits, domain_label)
+                running_D_loss+= d_loss.item()
+                d_loss.backward()
                 self.optimizer_D.step()
 
                 # Step 2 : 訓練Feature Extractor和Domain Classifier
                 class_logits = self.label_predictor(feature_extractor[:source_data.shape[0]])
                 domain_logits = self.domain_classifier(feature_extractor)
                 # loss為原本的class CE - lamb * domain BCE，相減的原因同GAN中的Discriminator中的G loss。
-                loss = self.class_criterion(class_logits, source_label) - lamb * self.domain_criterion(domain_logits, domain_label)
-                running_F_loss+= loss.item()
-                loss.backward()
+               
+                f_loss = self.class_criterion(class_logits, source_label) - lamb * self.domain_criterion(domain_logits, domain_label)
+                
+                running_F_loss+= f_loss.item()
+                f_loss.backward()
                 self.optimizer_F.step()
                 self.optimizer_C.step()
 
                 self.optimizer_D.zero_grad()
                 self.optimizer_F.zero_grad()
                 self.optimizer_C.zero_grad()
-
-                total_hit += torch.sum(torch.argmax(class_logits, dim=1) == source_label).item()
+                #total_hit += torch.sum(torch.argmax(class_logits, dim=1) == source_label).item()    
+                total_hit += torch.sum(torch.argmax(class_logits, dim=1) == torch.argmax(source_label, dim=1)).item()
                 total_num += source_data.shape[0]
 
                 #return running_D_loss / (i+1), running_F_loss / (i+1), total_hit / total_num
@@ -89,8 +98,14 @@ class DomainAdaptationTrainer:
                     torch.save(self.feature_extractor.state_dict(), f"../weight/P{self.patient}_extractor_model.bin")
                     torch.save(self.label_predictor.state_dict(), f"../weight/P{self.patient}_predictor_model.bin")
             if epoch%self.interval==0:
+                
+                # for name, param in self.label_predictor.named_parameters():
+                #     if param.grad is not None:
+                #         print(f'Parameter: {name}, Gradient norm: {param.grad.norm()}')
+                # for name, param in self.label_predictor.named_parameters():
+                #     if param.grad is not None:
+                #         print(f'Parameter: {name}, Gradient norm: {param.grad.norm()}')
                 print('epoch {:>3d}: train D loss: {:6.4f}, train F loss: {:6.4f}, acc {:6.4f}'.format(epoch, train_D_loss, train_F_loss, train_acc))
-        
 
 
     def evaluate(self, dataset, test_patients):
@@ -120,6 +135,28 @@ class DomainAdaptationTrainer:
         else:
             self.acc1, self.sensitivity1, self.specificity1= metrics(true_labels ,pred_labels)
         return self
+    def only_prediction(self, dataset):
+        # Evaluation code here
+        pred_labels = []
+        true_labels = []
+        # Loading model state dicts and setting to evaluation mode
+        self.label_predictor.load_state_dict(torch.load(f"../weight/P{self.patient}_predictor_model.bin"))
+        self.feature_extractor.load_state_dict(torch.load(f"../weight/P{self.patient}_extractor_model.bin"))
+        self.label_predictor.eval()
+        self.feature_extractor.eval()
+        for i, (test_data, test_label) in enumerate(dataset):
+            test_data = test_data.to(self.device)
+            class_logits = self.label_predictor(self.feature_extractor(test_data))
+            pred_label = torch.argmax(class_logits, dim=1).cpu().detach().numpy()
+            pred_labels.append(pred_label)
+            true_labels.append(test_label.cpu().numpy())
+    
+        
+        pred_labels= np.concatenate(pred_labels)
+        true_labels= np.concatenate(true_labels)
+        
+
+        return pred_labels
 
 class LSTMTrainer:
     def __init__(self,patient,  model, train_dataloader, val_dataloader, test_dataloader, \
@@ -143,13 +180,15 @@ class LSTMTrainer:
         self.model.eval()
 
         with torch.no_grad():
-            for data, label in self.test_dataloader:
-                data= data.cuda()
-                label= label.cuda()
+            for data, target in self.test_dataloader:
+                target =F.one_hot( target , num_classes=2)
+                target = target .type(torch.FloatTensor)
+                data, target = data.to(self.device), target.to(self.device)
                 output, _ = self.model(data)
+                target= target.argmax(dim=1)
                 pred_label = torch.argmax(output, dim=1).cpu().detach().numpy()
                 pred_labels.append(pred_label)
-                true_labels.append(label.cpu().numpy())
+                true_labels.append(target.cpu().numpy())
         
         pred_labels= np.concatenate(pred_labels)
         true_labels= np.concatenate(true_labels)
@@ -174,6 +213,8 @@ class LSTMTrainer:
             self.model.train() 
             
             for i,( data, target )in enumerate( self.train_dataloader):
+                target =F.one_hot( target , num_classes=2)
+                target = target .type(torch.FloatTensor)
                 data, target = data.to(self.device), target.to(self.device)
                 output,_ = self.model(data) 
                 loss = self.criterion(output, target) 
@@ -182,6 +223,7 @@ class LSTMTrainer:
                 loss.backward()  
                 self.optimizer.step()
                 y_pred = output.argmax(dim=1)
+                target= target.argmax(dim=1)
                 num_correct += (y_pred == target).sum().item()
                 train_len+= len(target)
 
@@ -193,11 +235,14 @@ class LSTMTrainer:
             self.model.eval()
             with torch.no_grad():
                 for i,( data, target )in enumerate( self.val_dataloader):
+                    target =F.one_hot( target , num_classes=2)
+                    target = target .type(torch.FloatTensor)
                     data, target = data.to(self.device), target.to(self.device)
                     output,_ = self.model(data) 
                     loss = self.criterion(output, target) 
                     val_loss+=loss
                     y_pred = output.argmax(dim=1)
+                    target= target.argmax(dim=1)
                     val_num_correct += (y_pred == target).sum().item()
                     val_len+= len(target)
 
